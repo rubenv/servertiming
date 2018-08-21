61a1a5ec942b946fc53391c5d6b80c1124b3ba26
Ruben Vermeersch <ruben@rocketeer.be>
2018-08-21 11:16:18 +0200

    Implement header formatting

--- servertiming.go
+++ servertiming.go
@@ -1,6 +1,22 @@
 package servertiming
 
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+)
+
 type Timing struct {
+	itemLock sync.Mutex
+	items    []*item
+}
+
+type item struct {
+	Name        string
+	Description string
+	Duration    time.Duration
+	Started     time.Time
 }
 
 func New() *Timing {
@@ -8,5 +24,59 @@ func New() *Timing {
 }
 
 func (t *Timing) String() string {
-	return ""
+	t.itemLock.Lock()
+	defer t.itemLock.Unlock()
+
+	parts := make([]string, 0)
+	for _, item := range t.items {
+		subParts := []string{item.Name}
+		if item.Description != "" {
+			subParts = append(subParts, fmt.Sprintf("desc=%#v", item.Description))
+		}
+		if item.Duration != 0 {
+			subParts = append(subParts, fmt.Sprintf("dur=%.2f", item.Duration.Seconds()*1000))
+		}
+
+		parts = append(parts, strings.Join(subParts, ";"))
+	}
+
+	return strings.Join(parts, ", ")
+}
+
+func (t *Timing) add(name, description string) *item {
+	t.itemLock.Lock()
+	defer t.itemLock.Unlock()
+
+	i := &item{
+		Name:        name,
+		Description: description,
+	}
+	t.items = append(t.items, i)
+
+	return i
+}
+
+func (t *Timing) Add(name, description string, duration time.Duration) {
+	i := t.add(name, description)
+	i.Duration = duration
+}
+
+func (t *Timing) Start(name, description string) {
+	i := t.add(name, description)
+	i.Started = time.Now()
+}
+
+func (t *Timing) Stop(name string) {
+	t.itemLock.Lock()
+	defer t.itemLock.Unlock()
+
+	for _, item := range t.items {
+		if item.Name == name {
+			item.Duration = time.Since(item.Started)
+		}
+	}
+}
+
+func (t *Timing) AddFlag(name, description string) {
+	t.add(name, description)
 }
--- servertiming_test.go
+++ servertiming_test.go
@@ -1,7 +1,10 @@
 package servertiming
 
 import (
+	"strconv"
+	"strings"
 	"testing"
+	"time"
 
 	"github.com/cheekybits/is"
 )
@@ -11,4 +14,38 @@ func TestString(t *testing.T) {
 
 	ti := New()
 	is.NotNil(ti)
+
+	ti.AddFlag("missedCache", "")
+	is.Equal(ti.String(), "missedCache")
+
+	ti = New()
+	ti.AddFlag("missedCache", "Cache missed")
+	is.Equal(ti.String(), "missedCache;desc=\"Cache missed\"")
+
+	ti = New()
+	ti.AddFlag("missedCache", "Cache missed: \"3\"")
+	is.Equal(ti.String(), `missedCache;desc="Cache missed: \"3\""`)
+
+	ti = New()
+	ti.AddFlag("missedCache", "")
+	ti.Add("cache", "Cache Read", 23200*time.Microsecond)
+	is.Equal(ti.String(), `missedCache, cache;desc="Cache Read";dur=23.20`)
+
+	ti = New()
+	ti.Start("db", "Database")
+	time.Sleep(10 * time.Millisecond)
+	ti.Stop("db")
+	s := ti.String()
+	parts := strings.Split(s, ";")
+	is.Equal(len(parts), 3)
+	is.Equal(parts[0], "db")
+	is.Equal(parts[1], `desc="Database"`)
+
+	tparts := strings.Split(parts[2], "=")
+	is.Equal(len(tparts), 2)
+	is.Equal(tparts[0], "dur")
+
+	d, err := strconv.ParseFloat(tparts[1], 64)
+	is.NoErr(err)
+	is.True(d > 10)
 }
